f8013b495d883197aed5b029a410e4f04a2ab4bc
Denzel Turambi <dturambi26@gmail.com>
2023-08-07 11:00:54 -0600

    feat: Create POST request function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,19 @@
 export const getOrders = () => {
   return fetch("http://localhost:3001/api/v1/orders").then((response) => response.json());
 };
+
+export const postOrder = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    body: JSON.stringify({
+      id: order.id,
+      name: order.name,
+      ingredients: order.ingredients
+    }),
+    headers: {
+      "Content-type": "application.json"
+    }
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
